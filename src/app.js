033e7ad99a5eb7edcc25e5e7bbd8df944f38c03e
Luis Pincay Toala <luis@MacBook-Pro-de-Luis.local>
2023-12-15 15:38:39 -0500

    testeando una primera ruta para estudiantes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const config = require('./config');
 
+const estudiantes = require('./modulos/estudiantes/rutas');
+
 const app = express();
 
 //configuración
 app.set('port', config.app.port);
+
+//rutas
+app.use('/api/estudiantes', estudiantes);
 
 module.exports = app;
