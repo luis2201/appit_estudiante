561e49bdbe7d72effb802ed53915c4346b14ba2b
Luis Pincay Toala <luis@MacBook-Pro-de-Luis.local>
2023-12-15 11:42:44 -0500

    testeando app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("hello world");
+});
+
+app.listen(3000, () => {
+    console.log("servidor escuchando por el puerto ", 3000);
+});
